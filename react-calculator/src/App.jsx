ef8515b3fbb560252fc6024a7c87d201e28d5c06
Twincasper <justinwatson651@gmail.com>
2024-03-05 14:52:18 -0500

    Created basic structure of calculator and css grid rules

---
+++ react-calculator/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">100,000</div>
+        <div className="current-operand">234,500</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className='span-two'>=</button>
+    </div>
+  )
+}
+
+export default App
